bbadb5cbb06de28015ce8fb8a1ac4d217f35c1ed
Jeison Caroly <jeison.casonatti@gmail.com>
2022-02-03 09:49:06 -0300

    Implementado String e Char (ainda nao oficial)

--- E1/teste.c
+++ E1/teste.c
@@ -2,10 +2,17 @@
 
 int main() {
   "svsdvsdv"
-  sdvsdvs
+  "testando o espaco"
+  "int x = 15;"
+  ""
+  >>
+  <<
+  int x = 15;
+  sdASKd_asD_
+  int
   bool
   bool
-  bool
+  char
   +2000
   -66666
   4444
@@ -13,5 +20,14 @@ int main() {
   44.33
   44.5e2
   44.
+  .50
   44.11E-2
+  'a'
+  '1'
+  '['
+  ' '
+  '"'
+  'asd'
+  t
+  f
 }
